3c77582368aca98a02a7ca186a811647411debcb
hyeleeeeen <heo22969087@gmail.com>
2024-08-05 17:13:51 +0900

    404오류 수정,console 삭제

--- src/app/(afterLogin)/[username]/_lib/getUserPosts.ts
+++ src/app/(afterLogin)/[username]/_lib/getUserPosts.ts
@@ -1,13 +1,15 @@
-import { QueryFunction } from "@tanstack/react-query";
 import { Post } from '@/model/Post'
+import { QueryFunction } from "@tanstack/query-core";
 
 export const getUserPosts: QueryFunction<Post[], [_1:string,_2:string, string]> = async({ queryKey }) => {
     const [_1, _2, username] = queryKey;
-    const res = await fetch(`http://localhost:9090/api/users/${username}/posts`, { // handler파일과 연동
+    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/users/${username}/posts?cursor=0`, {
         next: {
             tags: ["posts", "users", username], // 지정된 것만 새로고침
         }, // 서버쪽 캐싱
+        cache: 'no-store',
     });
+    console.log(res);
 
     if (!res.ok) {
         throw new Error("Failed to fetch data");
